7c27b3cd69376191b91fafe9cda501243fe8a367
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-11-12 16:01:19 +0000

    display blog posts
    Co-authored-by: Josh <joshua.hart@protonmail.com>

--- blog.11ty.js
+++ blog.11ty.js
@@ -3,6 +3,24 @@ exports.data = {
   title: "Blog",
 };
 
-exports.render = () => {
-  return /*html*/ `<h1>Blog</h1>`;
+exports.render = (data) => {
+  const posts = data.collections.blog;
+  let blogsHTML = "";
+
+  posts.forEach((post) => {
+    blogsHTML += /*html*/ `
+    <li>
+        <h2>${post.data.title}</h2>
+        <p>${post.template.frontMatter.content}</p>
+    </li>`;
+  });
+
+  console.log("**blogsHTML**:", blogsHTML);
+
+  return /*html*/ `
+        <h1>Blog</h1>
+        <ul>
+            ${blogsHTML}
+        </ul>
+    `;
 };
